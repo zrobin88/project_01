100e906eddd94ac888518d390b207425473e4a20
Zachary Robin <zacharyrobin@zacharys-mbp-2.home>
2019-01-20 13:32:00 -0500

    working last fm api with click event and user input

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,10 +1,23 @@
  
- let lastFmURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=metallica&api_key=43e2eac1bdb3ea4e9d978121427666c0&format=json"
+ 
+ $("#artist-submit").on("click", function(){
+ 
+let userInput = $("#artist-input").val();
 
- //ajax call to api 
+ let lastFmURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+userInput+"&api_key=43e2eac1bdb3ea4e9d978121427666c0&format=json"
+
+ //ajax call to last.fm api 
  $.ajax({
     url: lastFmURL,
     method: "GET"
   }).then(function (response) {
-    console.log(response)
+    //loop through top 50 tracks to get top 10 
+    for (let i = 0; i < 10; i++) {
+    let topTracks = response.toptracks.track[i].name
+    console.log(topTracks);
+        $("#top-tracks").append(topTracks);
+
+    }
+
   }); 
+});
