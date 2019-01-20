e9ac4e88a505567367321faa12b174e4e25c736f
craigz123 <czimbler60@gmail.com>
2019-01-20 15:57:10 -0500

    bands in town api is called. needs html refinments

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,10 +1,25 @@
- 
- let lastFmURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=metallica&api_key=43e2eac1bdb3ea4e9d978121427666c0&format=json"
 
- //ajax call to api 
- $.ajax({
-    url: lastFmURL,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response)
-  }); 
+let lastFmURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=metallica&api_key=43e2eac1bdb3ea4e9d978121427666c0&format=json"
+
+//ajax call to Last FM api 
+$.ajax({
+  url: lastFmURL,
+  method: "GET"
+}).then(function (response) {
+  console.log(response)
+});
+//ajax call to Bands In Town api
+$( "#artist-submit" ).click(function() {
+  alert( "Handler for .click() called." );
+
+let artist = $("#artist-input").val().trim();
+let bandsInTownEvents = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp&date=upcoming`
+let bandsInTownArtist = `https://rest.bandsintown.com/artists/${artist}?app_id=codingbootcamp`
+$.ajax({
+  url: bandsInTownEvents,
+  method: "GET"
+}).then(function (response) {
+  console.log(response)
+  $('#events').text(response[0].datetime);
+});
+});
