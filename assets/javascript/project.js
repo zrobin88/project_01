ca12fa321dcd7867a90972b9d37aaf55d2932660
chevaneze <cpsanon@gmail.com>
2019-01-22 20:15:49 -0500

    something new

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,9 +1,39 @@
-let lastFmURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=metallica&api_key=43e2eac1bdb3ea4e9d978121427666c0&format=json"
+//ajax call to Bands In Town api
 
-//ajax call to api 
-$.ajax({
- url: lastFmURL,
- method: "GET"
-}).then(function (response) {
- console.log(response)
-}); 
+$( "#artist-submit" ).click(function() {
+ 
+   
+   let artist = $("#artist-input").val().trim();
+   let bandsInTownEvents = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp&date=upcoming`
+   let bandsInTownArtist = `https://rest.bandsintown.com/artists/${artist}?app_id=codingbootcamp`
+   $.ajax({
+    url: bandsInTownEvents, 
+    method: "GET"
+   }).then(function (response) {
+    console.log(response);
+   
+    var info = {
+   date: "response[i].datetime",
+   ticketURL: "response[i].offers[0].url",
+   venueName: "response[i].venue.name",
+   venueCity: "response[i].venue.city",
+   venueCountry:  "response[i].venue.country",
+    };
+
+for (let i =0; i > 5; i++ ){
+    console.log(response[i]);
+    $("#events").text(response[i]);
+}
+
+
+   });
+   });
+
+    //$("#events").text(response[0].offers[0].on_sale_datetime);
+    //response[0].datetime
+   // response[0].offers[0].on_sale_datetime***
+   // response[0].offers[0].url
+   // response[0].venue.name
+   // response[0].venue.city
+    // response[0].venue.region**
+    // response[0].venue.country
