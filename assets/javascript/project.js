685c12c0e9228f0aeb29a4c30ff14089e5cfef7c
craigz123 <czimbler60@gmail.com>
2019-01-22 21:17:33 -0500

    don't use this code, use Chavaneze's

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,22 +1,34 @@
-
 //ajax call to Bands In Town api
-$( "#artist-submit" ).click(function() {
-  alert( "Handler for .click() called." );
 
-let artist = $("#artist-input").val().trim();
-let bandsInTownEvents = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp&date=upcoming`
-let bandsInTownArtist = `https://rest.bandsintown.com/artists/${artist}?app_id=codingbootcamp`
-$.ajax({
-  url: bandsInTownEvents,
-  method: "GET"
-}).then(function (response) {
-  console.log(response)
+$("#artist-submit").click(function () {
 
-   
 
-  for (let i = 0; i < 5; i++) {
-    console.log(response[i]);
-  }
+   let artist = $("#artist-input").val().trim();
+   let bandsInTownEvents = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp&date=upcoming`
+   let bandsInTownArtist = `https://rest.bandsintown.com/artists/${artist}?app_id=codingbootcamp`
+   $.ajax({
+       url: bandsInTownEvents,
+       method: "GET"
+   }).then(function (response) {
+       // console.log(response);
 
-});
+       // var info = {
+       //     date: response[i].datetime,
+       //     ticketURL: response[i].offers[0].url,
+       //     venueName: response[i].venue.name,
+       //     venueCity: response[i].venue.city,
+       //     venueCountry: response[i].venue.country,
+       // };
+
+       for (let i = 0; i < 5; i++) {
+           console.log(response[i]);
+        $("#events").append(response[i].datetime);
+        $("#events").append(response[i].offers[0].url);
+        $("#events").append(response[i].venue.name);
+        $("#events").append(response[i].venue.city);
+        $("#events").append(response[i].venue.country);
+       }
+
+
+   });
 });
