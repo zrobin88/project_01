117d45dd51131296099cd8963f3a5a69b7afb0e3
chevaneze <cpsanon@gmail.com>
2019-01-26 05:58:46 -0500

    tyuio

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,4 +1,44 @@
-//ajax call to Bands In Town api
+/
+
+
+var topTracksResponse;
+var artistInfoResponse;
+var artistInfo;
+var artistImg;
+var bandName;
+
+
+//on-click event for the Artist Tab that loads Artist data as well as top track listings and clears all previous dynamic html
+$("#artist-tab").on("click", function () {
+  $("#artist-info").html('');
+  $("#track-dump").html('');
+  $("#band-picture").html('');
+  $("#band-name").html('');;
+
+  track_dump_div = $("#track-dump");
+
+  track_dump_div.append(`<h2 class="flow-text center-align white-text">What to listen to</h2>`)
+  track_dump_div.append(`<ul>`)
+
+  for (let i = 0; i < 10; i++) {
+    track_dump_div.append(`<li class="white-text center-align" style="margin-left: 10px">
+       ${topTracksResponse.toptracks.track[i].name}
+    </li> `)
+  }
+
+  track_dump_div.append("</ul>")
+
+
+  $("#artist-info").append(artistInfo);
+  $("#band-picture").append(`<img class="responsive-img" src="${artistImg}"/>`);
+  $("#band-name").append(`<h2 class="flow-text center-align">${bandName}</h2>`);
+
+});
+
+//on-click event for the Events Tab that loads Events data and clears all previous dynamic html
+$("#events-tab").on("click", function () {
+
+  //ajax call to Bands In Town api
 
 $("#artist-submit").click(function () {
 
@@ -11,7 +51,7 @@ $("#artist-submit").click(function () {
       url: bandsInTownEvents,
       method: "GET"
   }).then(function (response) {
-    console.log(response);
+  
      
     
 let myTicketLink= response[0]["url"]
@@ -20,7 +60,7 @@ $("#dateTime").append(response[0].datetime);
 $("#venueName").append(response[0].venue.name);
 $("#venueCity").append(response[0].venue.city);
 $("#venueCountry").append(response[0].venue.country);
-$("#offersUrl").append(`<button><a href = ${myTicketLink}; target="_blank">Purchase Tickets Now</a></button>`);
+$("#offersUrl").append(`<button><a href = ${myTicketLink}; target="_blank" class="tickets">Purchase Tickets Now</a></button>`);
     
 let myTicketLinkTwo= response[1]["url"]
    
@@ -46,4 +86,4 @@ $("#offersUrlThree").append(`<button><a href = ${myTicketLinkThree};">Purchase T
 
 
 
-
+});
